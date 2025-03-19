596324ac96b5d55dd26835d49963bb79d9d06c62
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-03-19 23:50:25 +0400

    Lesson 1

---
+++ src/main/java/org/example/lesson1/Main.java
@@ -0,0 +1,23 @@
+package org.example.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World Excample");
+
+
+
+
+        // JDK JRE JVM
+
+
+
+        // 1.COMPILE
+        // 2.JVM
+        // 3.JRE
+        // VƏ VƏ İNTERPREATOR
+
+
+    }
+}
+
