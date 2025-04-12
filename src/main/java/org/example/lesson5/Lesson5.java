cb27b31997cb5e8a9d8798346bd1a19399b56109
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-04-13 01:07:19 +0400

    Lesson 5

---
+++ src/main/java/org/example/lesson5/Lesson5.java
@@ -0,0 +1,22 @@
+package org.example.lesson5;
+
+public class Lesson5 {
+
+    public static void main(String[] args) {
+
+        // byte sort int long double float char boolean
+
+        // String
+
+
+        // immutable
+
+        String text = "Hello World";
+
+
+        String text3 = new String("Hello World");
+        String text4=new String("Hello World");
+
+
+    }
+}
