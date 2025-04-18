62f18d81a63956cdfc5c7e6760e1d2aa15d58c81
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-04-18 15:33:58 +0400

    Lesson 5 Practic

---
+++ src/main/java/org/example/lesson5/Exercise.java
@@ -0,0 +1,52 @@
+package org.example.lesson5;
+
+public class Exercise {
+
+    // String ilə bağlı tapşırıqlar
+
+     /*   1. "repeat me" sözünü String funksiyası ilə 4 dəfə təkrarla.
+
+        2. Verilmiş sözləri (apple, banana, cherry) “,”ilə birləşdir: →apple, banana, cherry
+
+        3. "HeLLo WoRLd" mətnini həm bütün hərfləri kiçik, həm də bütün hərfləri böyük formada göstər.
+
+        4. " hello world " cümləsindəki başlanğıc və sonundakı boşluqları sil və nəticəni çap et →hello world
+
+        5. "apple, banana, cherry" mətnində “cherry”sözünü “grapes”ilə əvəz et.
+
+        6. "this is a text" cümləsindəki 5 - ci simvolu “x”ilə əvəz et.
+
+      */
+
+
+    // Asan tapşırıqlar
+      /*
+
+        1. double tipində iki ədəd təyin et və fərqini çap et.
+
+        2. boolean tipində bir dəyişəni String tipinə çevir.
+
+        3. char tipində bir simvol təyin et və onun ASCII dəyərini çap et.
+
+        4. byte tipində bir dəyişən təyin et və onu int tipinə çevir.
+
+        5. double ədədin kvadratını hesabla və çap et.*/
+
+
+    // Nisbətən çətin səviyyəli tapşırıqlar
+
+   /*   1. İstifadəçidən String tipində ad və int tipində yaş daxil et, sonra "Salam, [ad], sən [yaş] yaşındasan"
+             kimi çap et.
+
+        2. char tipində bir hərf təyin et, onu String tipinə çevir və çap et.
+
+        3. String tipində tarix daxil et  və onu int -ə çevir, 1 il artır, çap et.
+
+        4. İki String dəyişəni birləşdir və nəticəni length() metodu ilə uzunluğunu çap et.
+
+        5. String tipində ədəd yaz (məs:"123")və onu int tipinə çevir.
+
+    */
+
+
+}
---
+++ src/main/java/org/example/lesson5/Lesson5Part2Pratic.java
@@ -0,0 +1,15 @@
+package org.example.lesson5;
+
+public class Lesson5Part2Pratic {
+    public static void main(String[] args) {
+
+        String s1 = "1025";
+
+
+        int eded = Integer.parseInt(s1);
+
+        System.out.println(eded);
+
+
+    }
+}
